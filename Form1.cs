ac9ab22838f60f893e84207e294a0408cdcd587f
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-20 18:24:57 -0400

    use api info class

--- Form1.cs
+++ Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.Toolkit.Uwp.Notifications;
 using System.Net;
+using GitactionAPI;
 namespace Gitacations
 {
     public partial class Form1 : Form
@@ -35,36 +36,7 @@ namespace Gitacations
 
         private void checkAPIStatusToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            string api_url = "https://api.github.com/users/techpenguineer/subscriptions";
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(api_url);
-            request.Timeout = 15000;
-            request.Method = "HEAD";
-
-            try
-            {
-                using (HttpWebResponse response = (HttpWebResponse) request.GetResponse())
-                {
-                    if(response.StatusCode == HttpStatusCode.OK)
-                    {
-                        new ToastContentBuilder()
-                            .AddText("API Status Request")
-                            .AddText("API Status returned as OK")
-                            .Show();
-                    } else 
-                    {
-                        new ToastContentBuilder()
-                              .AddText("API Status Request")
-                              .AddText("API Status returned dead")
-                               .Show();
-                    }
-                }
-            }catch(WebException)
-            {
-                new ToastContentBuilder()
-                  .AddText("API Status Request")
-                  .AddText("There was an error trying to request the api")
-                  .Show();
-            }
+            GitactionAPI.apiINFO.returnApiStatus();
         }
     }
 }
---
+++ api/get_status.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Uwp.Notifications;
+using System.Net;
+
+namespace GitactionAPI
+{
+    public class apiINFO
+    {
+
+        public static void returnApiStatus()
+        {
+            string api_url = "https://api.github.com/users/techpenguineer/subscriptions";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(api_url);
+            request.Timeout = 15000;
+            request.Method = "HEAD";
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        new ToastContentBuilder()
+                            .AddText("API Status Request")
+                            .AddText("API Status returned as OK")
+                            .Show();
+                    }
+                    else
+                    {
+                        new ToastContentBuilder()
+                              .AddText("API Status Request")
+                              .AddText("API Status returned dead")
+                               .Show();
+                    }
+                }
+            }
+            catch (WebException)
+            {
+                new ToastContentBuilder()
+                  .AddText("API Status Request")
+                  .AddText("There was an error trying to request the api")
+                  .Show();
+            }
+        }
+    }
+}
+
+
+
