decc82d97bcdea9bacf1f530db267a123accceab
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-20 18:28:27 -0400

    make url parser api string

---
+++ api/parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitactionAPI
+{
+    public class parser
+    {
+        public static void parseApiInfo(string Username)
+        {
+            string api_url = $"https://api.github.com/users/{Username}/subscriptions";
+        }
+    }
+}
