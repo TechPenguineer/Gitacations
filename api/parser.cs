ad866972c54a54d3503e771130fb6342b12efffe
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-07-20 18:30:55 -0400

    install json parser package

--- api/parser.cs
+++ api/parser.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Net;
 namespace GitactionAPI
 {
     public class parser
@@ -11,6 +11,8 @@ namespace GitactionAPI
         public static void parseApiInfo(string Username)
         {
             string api_url = $"https://api.github.com/users/{Username}/subscriptions";
+
+
         }
     }
 }
